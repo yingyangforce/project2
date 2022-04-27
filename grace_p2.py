fa2025d311a5ad5d35f1dbe1522586473009cea8
Gene Calmer <yingyangforce@gmail.com>
2022-04-27 01:41:30 -0400

    FEAT: added usr_input loop

---
+++ grace_p2.py
@@ -0,0 +1,18 @@
+# File: grace_p2.py
+# Author: Isaiah Grace
+# Date: 2022/4/19
+# Lab Section: Tuesday
+# Email: isaiah.grace@maine.edu
+# Description: Retail customer database manager
+# Collabs: N/a
+
+# --- Project 2 ---
+
+def inputloop():
+    usr_input = input("Please enter an option ([q] to quit)): ")
+
+    while usr_input != 'q':
+        usr_input = input("Please enter an option ([q] to quit)): ") 
+        print('\n')
+
+    print("Goodbye")
